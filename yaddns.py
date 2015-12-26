4eb4d669b5768ac2a1ca2b225a2712281aa5ba29
K900 <k0009000@gmail.com>
2015-12-26 18:54:55 +0300

    Add the actual source code
    It works. Yay.

---
+++ yaddns.py
@@ -0,0 +1,49 @@
+import requests
+import click
+
+PDD_BASE = 'https://pddimp.yandex.ru/'
+
+
+@click.command()
+@click.option('--domain', help='Base domain name')
+@click.option('--subdomain', help='Subdomain name', default='ddns')
+@click.option('--token', help='PddToken auth token (get one here: https://pddimp.yandex.ru/api2/admin/get_token)')
+def main(domain, subdomain, token):
+    ip_address = requests.get('https://api.ipify.org').text
+    print('Your public facing IP address is', ip_address)
+
+    def pdd_get(url, params):
+        return requests.get(PDD_BASE + url, params, headers={
+            'PddToken': token
+        }).json()
+
+    def pdd_post(url, params):
+        return requests.post(PDD_BASE + url, params, headers={
+            'PddToken': token
+        }).json()
+
+    for record in pdd_get('/api2/admin/dns/list', {'domain': domain})['records']:
+        if record['type'] == 'A' and record['subdomain'] == subdomain:
+            print('Found existing record, updating...')
+            print('Existing record ID:', record['record_id'])
+            pdd_post('/api2/admin/dns/edit', {
+                'record_id': record['record_id'],
+                'domain': domain,
+                'type': 'A',
+                'content': ip_address,
+                'subdomain': subdomain
+            })
+            break
+    else:
+        print("Didn't find a record, creating...")
+        pdd_post('/api2/admin/dns/add', {
+            'domain': domain,
+            'type': 'A',
+            'content': ip_address,
+            'subdomain': subdomain
+        })
+    print('Done!')
+
+
+if __name__ == "__main__":
+    main()
